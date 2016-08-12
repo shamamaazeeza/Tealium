117067fc07a2acdf5404add2c093fadb1b2956f7
Juan Leon <jleon@us.ibm.com>
2016-08-12 15:37:20 -0500

    Initial version from Tealium

--- common/js/coremetrics/add_utag.js
+++
@@ -1,4 +0,0 @@
-var s = document.createElement('script');
-    s.setAttribute('type', 'text/javascript');
-    s.setAttribute('src', '//tags.tiqcdn.com/utag/ibm/main/prod/utag.js');
-    document.body.appendChild(s);
