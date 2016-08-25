4ed477c5d318ed69d081fa4f3c591132b2f1c832
Juan Leon <jleon@us.ibm.com>
2016-08-25 18:54:50 -0500

    TME-Datalayer-20160824-2

--- tealium/digitalanalytics/extension/digitalanalytics-check_eluminate_duplicate-cust.js
+++ tealium/digitalanalytics/extension/digitalanalytics-check_eluminate_duplicate-cust.js
@@ -1,11 +1,12 @@
 /*
- * Id            : tm-v1.0/tealium/digitalanalytics/extension/digitalanalytics-check_eluminate_duplicate-cust.js
- * Extension Name: digitalanalytics-core-pre.js
- * Scope         : Pre Loader
+ * Id            : tm-v1.0/tealium/digitalanalytics/extension/digitalanalytics-is_eluminate_loaded-cust.js
+ * Extension Name: digitalanalytics-is_eluminate_loaded-cust.js
+ * Scope         : All Coremetrics Tags
  * Execution     : N/A
  * 
  * =====|| NOTE: DO NOT MODIFY THIS SCRIPT IN TEALIUM, UPDATE SVN VERSION IN ECLIPSE
  */
+var tmeid="digitalanalytics-is_eluminate_loaded-cust.js";
 window.eluminate_loaded = false;
 if (typeof(cmCreateElementTag) == "function" && typeof(cmCreateElementTag.isGhost) == "undefined" ) {
   window.eluminate_loaded=true;
